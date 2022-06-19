58fca7a37ed23f7c2de40e2e6fa4a8e5e6fcad80
devopsseekerscloud <devops.seekerscloud@gmail.com>
2022-06-18 21:40:45 -0700

    Initial commit

---
+++ src/AppInitializer.java
@@ -0,0 +1,38 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class AppInitializer {
+    public static void main(String[] args) {
+        AppInitializer app = new AppInitializer();
+        app.saveCustomer();
+    }
+
+    private void saveCustomer() {
+        Customer c1 = new Customer("1","Nimal","Colombo",25000);
+        Configuration configuration=
+                new Configuration().configure("hibernate.cfg.xml")
+                        .addAnnotatedClass(Customer.class);
+        SessionFactory sessionFactory= configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(c1);
+        transaction.commit();
+        session.close();
+        sessionFactory.close();
+    }
+
+    private void findCustomer() {
+    }
+
+    private void modifyCustomer() {
+    }
+
+    private void deleteCustomer() {
+    }
+
+    private void allCustomers() {
+    }
+
+}

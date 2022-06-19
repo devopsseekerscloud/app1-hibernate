ede8b60528754821b07450fafa234e9b3869ebe8
devopsseekerscloud <devops.seekerscloud@gmail.com>
2022-06-18 22:08:01 -0700

    4

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -2,6 +2,9 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import java.util.List;
 
 public class AppInitializer {
     public static void main(String[] args) {
@@ -9,11 +12,12 @@ public class AppInitializer {
         //app.saveCustomer();
         //app.findCustomer();
         //app.modifyCustomer();
-        app.deleteCustomer();
+        //app.deleteCustomer();
+        //app.allCustomers();
     }
 
     private void saveCustomer() {
-        Customer c1 = new Customer("1", "Nimal", "Colombo", 25000);
+        Customer c1 = new Customer("2", "Bandara", "Panadura", 45000);
         Configuration configuration =
                 new Configuration().configure("hibernate.cfg.xml")
                         .addAnnotatedClass(Customer.class);
@@ -57,13 +61,23 @@ public class AppInitializer {
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.openSession();
         Transaction transaction = session.beginTransaction();
-        session.delete(session.find(Customer.class,"1"));
+        session.delete(session.find(Customer.class, "1"));
         transaction.commit();
         session.close();
         sessionFactory.close();
     }
 
     private void allCustomers() {
+        Configuration configuration =
+                new Configuration().configure("hibernate.cfg.xml")
+                        .addAnnotatedClass(Customer.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Query query = session.createQuery("FROM Customer");// HQL;
+        List list = query.list();
+        System.out.println(list);
+        session.close();
+        sessionFactory.close();
     }
 
 }

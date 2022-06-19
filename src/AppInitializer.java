6d2df10797338676cccdda8abd7965b4bbe52a0a
devopsseekerscloud <devops.seekerscloud@gmail.com>
2022-06-18 21:52:36 -0700

    2

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -6,16 +6,17 @@ import org.hibernate.cfg.Configuration;
 public class AppInitializer {
     public static void main(String[] args) {
         AppInitializer app = new AppInitializer();
-       // app.saveCustomer();
-        app.findCustomer();
+        //app.saveCustomer();
+        //app.findCustomer();
+        app.modifyCustomer();
     }
 
     private void saveCustomer() {
-        Customer c1 = new Customer("1","Nimal","Colombo",25000);
-        Configuration configuration=
+        Customer c1 = new Customer("1", "Nimal", "Colombo", 25000);
+        Configuration configuration =
                 new Configuration().configure("hibernate.cfg.xml")
                         .addAnnotatedClass(Customer.class);
-        SessionFactory sessionFactory= configuration.buildSessionFactory();
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.openSession();
         Transaction transaction = session.beginTransaction();
         session.save(c1);
@@ -25,16 +26,27 @@ public class AppInitializer {
     }
 
     private void findCustomer() {
-        Configuration configuration=
+        Configuration configuration =
                 new Configuration().configure("hibernate.cfg.xml")
                         .addAnnotatedClass(Customer.class);
-        SessionFactory sessionFactory= configuration.buildSessionFactory();
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.openSession();
         Customer customer = session.find(Customer.class, "1");
         System.out.println(customer);
     }
 
     private void modifyCustomer() {
+        Customer c1 = new Customer("1", "Bandara", "Galle", 45000);
+        Configuration configuration =
+                new Configuration().configure("hibernate.cfg.xml")
+                        .addAnnotatedClass(Customer.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.update(c1);
+        transaction.commit();
+        session.close();
+        sessionFactory.close();
     }
 
     private void deleteCustomer() {

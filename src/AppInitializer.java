3af8fd5de148fe0c33e8d78ede86a1893c5cbea1
devopsseekerscloud <devops.seekerscloud@gmail.com>
2022-06-18 22:22:31 -0700

    5

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -18,66 +18,43 @@ public class AppInitializer {
 
     private void saveCustomer() {
         Customer c1 = new Customer("2", "Bandara", "Panadura", 45000);
-        Configuration configuration =
-                new Configuration().configure("hibernate.cfg.xml")
-                        .addAnnotatedClass(Customer.class);
-        SessionFactory sessionFactory = configuration.buildSessionFactory();
-        Session session = sessionFactory.openSession();
+        Session session = HibernateUtil.buildSessionFactory().openSession();
         Transaction transaction = session.beginTransaction();
         session.save(c1);
         transaction.commit();
         session.close();
-        sessionFactory.close();
     }
 
     private void findCustomer() {
-        Configuration configuration =
-                new Configuration().configure("hibernate.cfg.xml")
-                        .addAnnotatedClass(Customer.class);
-        SessionFactory sessionFactory = configuration.buildSessionFactory();
-        Session session = sessionFactory.openSession();
+        Session session = HibernateUtil.buildSessionFactory().openSession();
         Customer customer = session.find(Customer.class, "1");
         System.out.println(customer);
     }
 
     private void modifyCustomer() {
         Customer c1 = new Customer("1", "Bandara", "Galle", 45000);
-        Configuration configuration =
-                new Configuration().configure("hibernate.cfg.xml")
-                        .addAnnotatedClass(Customer.class);
-        SessionFactory sessionFactory = configuration.buildSessionFactory();
-        Session session = sessionFactory.openSession();
+        Session session = HibernateUtil.buildSessionFactory().openSession();
         Transaction transaction = session.beginTransaction();
         session.update(c1);
         transaction.commit();
         session.close();
-        sessionFactory.close();
     }
 
     private void deleteCustomer() {
-        Configuration configuration =
-                new Configuration().configure("hibernate.cfg.xml")
-                        .addAnnotatedClass(Customer.class);
-        SessionFactory sessionFactory = configuration.buildSessionFactory();
-        Session session = sessionFactory.openSession();
+
+        Session session = HibernateUtil.buildSessionFactory().openSession();
         Transaction transaction = session.beginTransaction();
         session.delete(session.find(Customer.class, "1"));
         transaction.commit();
         session.close();
-        sessionFactory.close();
     }
 
     private void allCustomers() {
-        Configuration configuration =
-                new Configuration().configure("hibernate.cfg.xml")
-                        .addAnnotatedClass(Customer.class);
-        SessionFactory sessionFactory = configuration.buildSessionFactory();
-        Session session = sessionFactory.openSession();
+        Session session = HibernateUtil.buildSessionFactory().openSession();
         Query query = session.createQuery("FROM Customer");// HQL;
         List list = query.list();
         System.out.println(list);
         session.close();
-        sessionFactory.close();
     }
 
 }
